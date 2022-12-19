13c3beea44cad6ba36be670bd722eb040cf26e95
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-19 23:14:43 +0500

    changes

---
+++ src/components/layout/content.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="content">
+    <div class="content__intro">
+      <div class="content__intro--details">
+        <h1>
+          Bring everyone <br />
+          together to build
+          <br />
+          better products.
+        </h1>
+        <p>
+          Manage makes it simple for software teams <br />
+          to plane day-to-day tasks while keeping the <br />larger team goals in
+          view.
+        </p>
+        <button class="btn">Get Started</button>
+      </div>
+      <div class="content__intro--image">
+        <img src="../../assets/images/illustration-intro.svg" alt="" />
+      </div>
+    </div>
+  </main>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,12 +1,14 @@
 <template>
   <headerVue />
+  <contentVue />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import headerVue from "./header.vue";
+import contentVue from "./content.vue";
 
 export default defineComponent({
-  components: { headerVue },
+  components: { headerVue, contentVue },
   setup() {
     return {};
   },
