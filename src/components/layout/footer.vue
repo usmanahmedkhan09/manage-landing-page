278d8608afd079e97479b99c2ad386f49cfe0de2
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-20 19:40:42 +0500

    footer is added

---
+++ src/components/layout/footer.vue
@@ -0,0 +1,54 @@
+<template>
+  <footer class="footer">
+    <div class="logo_wrapper">
+      <Logo class="logo" />
+      <div class="social-icons">
+        <IconYouTube />
+        <IconFaceBook />
+        <IconTwitter />
+        <IconInstagram />
+        <IconPrinterest />
+      </div>
+    </div>
+    <ul role="list">
+      <li>About Us</li>
+      <li>Contact</li>
+      <li>Blog</li>
+    </ul>
+    <ul role="list">
+      <li>Careers</li>
+      <li>Support</li>
+      <li>Privacy Policy</li>
+    </ul>
+    <div class="button_wrapper">
+      <div class="input__wrapper">
+        <input type="text" placeholder="Updates in your inbox..." />
+        <button class="btn">Go</button>
+      </div>
+      <p class="rights">Copyrights 2020. All Rights Reserved</p>
+    </div>
+  </footer>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Logo from "@/components/icons/logo-icon.vue";
+import IconYouTube from "@/components/icons/icon-youtube.vue";
+import IconFaceBook from "@/components/icons/icon-facebook.vue";
+import IconTwitter from "@/components/icons/icon-twitter.vue";
+import IconInstagram from "@/components/icons/icon-instagram.vue";
+import IconPrinterest from "@/components/icons/icon-pinterest.vue";
+
+export default defineComponent({
+  components: {
+    Logo,
+    IconYouTube,
+    IconFaceBook,
+    IconTwitter,
+    IconInstagram,
+    IconPrinterest,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,14 +1,16 @@
 <template>
   <headerVue />
   <contentVue />
+  <footerVue />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import headerVue from "./header.vue";
 import contentVue from "./content.vue";
+import footerVue from "./footer.vue";
 
 export default defineComponent({
-  components: { headerVue, contentVue },
+  components: { headerVue, contentVue, footerVue },
   setup() {
     return {};
   },
